4f362895c5f5e22b0328e2f19804f392b5d28243
rebeca <beca.cout@gmail.com>
2024-05-27 12:36:21 -0300

    delete backend folder

--- backend/src/api/controllers/knowledges/index.js
+++
@@ -1,6 +0,0 @@
-export { default as createKnowledge } from "./create-knowledge.js";
-export { default as deleteKnowledge } from "./delete-knowledge.js";
-export { default as getKnowledge } from "./get-knowledge.js";
-export { default as getAllKnowledge } from "./getall-knowledge.js";
-export { default as updateKnowledge } from "./update-knowledge.js";
-export { default as createQRCodeKnowledge } from "./create-qrcode-knowledge.js";
